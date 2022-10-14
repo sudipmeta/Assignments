f8634ab8a0109280168bfe436630752507f554c1
sudipmeta <115703075+sudipmeta@users.noreply.github.com>
2022-10-14 14:06:23 +0530

    Add files via upload

---
+++ Assignment1/Item.java
@@ -0,0 +1,20 @@
+package assignment1listarray;
+
+public class Item {
+	int itemid;
+	String name;
+	String description;
+	double price;
+	public Item(int itemid,
+	String name,
+	String description,
+	double price) {
+		this.itemid = itemid;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		
+		// TODO Auto-generated constructor stub
+	}
+
+}
