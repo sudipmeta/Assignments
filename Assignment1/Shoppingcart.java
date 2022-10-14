fc3ddc62883ecf6a603948b6d3966b71db38d335
sudipmeta <115703075+sudipmeta@users.noreply.github.com>
2022-10-14 15:46:36 +0530

    Update Shoppingcart.java

--- Assignment1/Shoppingcart.java
+++ Assignment1/Shoppingcart.java
@@ -2,25 +2,45 @@ package assignment1listarray;
 import java.util.*;
 
 public class Shoppingcart {
-	HashMap<String, Integer> mapcart = new HashMap<>();
-	double totalprice;
+	static HashMap<Item, Integer> mapcart = new HashMap<>();
+	double totalprice = 0;
 	int quantity;
+	double total;
+	
 	public void addToCart(Item Item, Integer quantity) {
-		mapcart.put(Item.name, quantity);
-		System.out.print(mapcart);
+		mapcart.put(Item, quantity);
+		System.out.println("Cart:");
+		showcart();
 	}
 	public void displayQty(Item Item) {
-		System.out.println("\nquantity of this item is"+mapcart.get(Item.name));
+		System.out.println("\nquantity of this item is"+mapcart.get(Item));
 	}
 	public void updateQty(Item Item, Integer quantity) {
-		mapcart.put(Item.name, quantity);
-		System.out.println("\nupdated cart"+ mapcart);
+		mapcart.put(Item, quantity);
+		System.out.println("\nupdated cart");
+		showcart();
 	}
 	public void deleteItem(Item Item) {
-		mapcart.remove(Item.name);
-		System.out.println("\n"+mapcart);
+		mapcart.remove(Item);
+		System.out.println("\n");
+		showcart();
 	}
-	
+	/*public double total(Item Item) {
+		total = Item.price*mapcart.get(Item);
+		return total;
+		
+	}*/
+	public void displayBill() {
+		for(Map.Entry<Item, Integer> entry: mapcart.entrySet()) {
+			totalprice+= entry.getKey().price*entry.getValue();
+		}
+		System.out.println("total price of the cart is"+ totalprice);
+	}
+	public void showcart() {
+		for(Map.Entry<Item, Integer> entry:mapcart.entrySet()) {
+			System.out.println(entry.getKey().name + ":" + entry.getValue());
+		}
+	}
 	
 	
 
